c18f813baf5204abf9a7081909a7e918501a1e37
FHOCFHOC <backintheusa@gmail.com>
2022-07-27 15:43:46 -0300

    fourth commit

---
+++ src/main/java/com/corinto/parking/repository/ParkingRepository.java
@@ -0,0 +1,10 @@
+package com.corinto.parking.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.corinto.parking.model.Parking;
+
+@Repository
+public interface ParkingRepository extends JpaRepository<Parking, String> {
+}
